80b951ad73b933ee39eab6ee2d5644b932c22040
Luan Lopes <luan@shareeat.com.br>
2019-11-03 17:37:16 -0300

    adjusts api name

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const v1Routes = require('./v1')
 
 router.get('/', (_req, res) => {
-  res.json({ message: 'Welcome to Class Management API! Choose one version' })
+  res.json({ message: 'Welcome to Course Management API! Choose one version' })
 })
 
 router.use('/v1', v1Routes)
